31e83e205fb0304e940dcddd60805e543f96501e
soumya <soumyachopra@gmail.com>
2022-03-15 22:58:37 +0530

    support for others

--- resume/views.py
+++ resume/views.py
@@ -12,9 +12,12 @@ def index(request):
     context = {}
     if search:
         query = SearchCandidate(search, option)
-        res_list = Resume.objects.raw(query)
-        paginator = Paginator(res_list, 6) # Show 6 resumes per page.
-        page_number = request.GET.get('page',1)
-        page_obj = paginator.get_page(page_number)
-        context = { 'page_obj': page_obj , 'query': query, 'search': search, 'option': option}
+        if option != 'sql':
+            context = {'query': query}
+        else:
+            res_list = Resume.objects.raw(query)
+            paginator = Paginator(res_list, 6) # Show 6 resumes per page.
+            page_number = request.GET.get('page',1)
+            page_obj = paginator.get_page(page_number)
+            context = { 'page_obj': page_obj , 'query': query, 'search': search, 'option': option}
     return render(request, 'resume/index.html', context)
